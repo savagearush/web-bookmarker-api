61b463639a6c1f19c41cc1d44a18d99f2a7c8f33
Arush Sharma <arush3339@gmail.com>
2023-07-15 17:52:05 +0530

    Made Some changes to mailer

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const morgan = require("morgan");
 const { join } = require("path");
 require("express-async-errors");
 const cors = require("cors");
-
+const helmet = require("helmet");
 config({ path: "config/config.env" });
 
 if (process.env.NODE_ENV == "development") app.use(morgan("tiny"));
 
 if (!process.env.JWT_PRIVATE_KEY) {
-  console.error("FATAL EnsvRORR : JWT_PRIVATE_KEY is not defined.");
+  console.error("FATAL ERROR : JWT_PRIVATE_KEY is not defined.");
   process.exit(1);
 }
 const corsOptions = {
@@ -19,6 +19,8 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 };
+
+app.use(helmet());
 app.use(cors(corsOptions));
 app.use(express.static(join(__dirname, "\\public")));
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +30,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server is Listening on PORT ${PORT}`);
 });
